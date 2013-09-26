3f3da6ca97af6329d14839402917bf3ab7ad5c01
Eduardo Garcia <eduardo.garcia@liferay.com>
2013-09-26 16:13:24 +0200

    LPS-37681 If the propagation of changes is disabled, site pages should be sortable without any restrictions

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceHelper.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceHelper.java
@@ -118,7 +118,8 @@ public class LayoutLocalServiceHelper implements IdentifiableBean {
 					groupId, privateLayout);
 
 				if (Validator.isNotNull(
-						layoutSet.getLayoutSetPrototypeUuid())) {
+						layoutSet.getLayoutSetPrototypeUuid()) &&
+					layoutSet.isLayoutSetPrototypeLinkEnabled()) {
 
 					priority = priority + _PRIORITY_BUFFER;
 				}
