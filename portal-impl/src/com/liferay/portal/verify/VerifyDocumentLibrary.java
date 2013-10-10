e7206ff3e0803f82e37453cd41fbbba2abc9b0a0
NorbertKocsis <norbert.kocsis@liferay.com>
2013-10-10 15:35:26 +0200

    LPS-37322 Log Warn VerifyDocumentLibrary if file/fileVersion are not found: Do not stop

--- portal-impl/src/com/liferay/portal/verify/VerifyDocumentLibrary.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyDocumentLibrary.java
@@ -140,10 +140,23 @@ public class VerifyDocumentLibrary extends VerifyProcess {
 
 				DLFileEntry dlFileEntry = (DLFileEntry)object;
 
-				InputStream inputStream =
-					DLFileEntryLocalServiceUtil.getFileAsStream(
+				InputStream inputStream = null;
+
+				try {
+					inputStream = DLFileEntryLocalServiceUtil.getFileAsStream(
 						dlFileEntry.getUserId(), dlFileEntry.getFileEntryId(),
 						dlFileEntry.getVersion(), false);
+				}
+				catch (Exception e) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"Unable to find file entry " +
+								dlFileEntry.getName(),
+							e);
+					}
+
+					return;
+				}
 
 				String title = DLUtil.getTitleWithExtension(
 					dlFileEntry.getTitle(), dlFileEntry.getExtension());
@@ -184,11 +197,27 @@ public class VerifyDocumentLibrary extends VerifyProcess {
 
 				DLFileVersion dlFileVersion = (DLFileVersion)object;
 
-				InputStream inputStream =
-					DLFileEntryLocalServiceUtil.getFileAsStream(
+				InputStream inputStream = null;
+
+				try {
+					inputStream = DLFileEntryLocalServiceUtil.getFileAsStream(
 						dlFileVersion.getUserId(),
 						dlFileVersion.getFileEntryId(),
 						dlFileVersion.getVersion(), false);
+					}
+				catch (Exception e) {
+					DLFileEntry fileEntry = dlFileVersion.getFileEntry();
+
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"Unable to find file version " +
+								dlFileVersion.getVersion() + " for file " +
+									"entry " + fileEntry.getName(),
+							e);
+					}
+
+					return;
+				}
 
 				String title = DLUtil.getTitleWithExtension(
 					dlFileVersion.getTitle(), dlFileVersion.getExtension());
