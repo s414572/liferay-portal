fe486a91a2361789ccdade7418e9777a4cfe6abf
Brian Chan <brian.chan@liferay.com>
2013-10-18 13:49:21 -0700

    LPS-41438 SF

--- portal-pacl/src/com/liferay/portal/security/pacl/PortalSecurityManagerImpl.java
+++ portal-pacl/src/com/liferay/portal/security/pacl/PortalSecurityManagerImpl.java
@@ -296,6 +296,7 @@ public class PortalSecurityManagerImpl extends SecurityManager
 		AccessController.checkPermission(permission);
 	}
 
+	@Override
 	public void destroy() {
 		synchronized (_originalSecurityManager) {
 			Policy.setPolicy(_portalPolicy.getOriginalPolicy());
