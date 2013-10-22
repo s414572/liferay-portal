6573371523dfe4ee91174dfeb7b5c1e4573c0234
Hugo Huijser <hugo.huijser@liferay.com>
2013-10-22 10:54:51 +0800

    SourceFormatter - improve

--- portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
+++ portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
@@ -1297,7 +1297,9 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 					String newIfClause = checkIfClause(
 						ifClause, fileName, lineCount);
 
-					if (!ifClause.equals(newIfClause)) {
+					if (!ifClause.equals(newIfClause) &&
+						content.contains(ifClause)) {
+
 						return StringUtil.replace(
 							content, ifClause, newIfClause);
 					}
