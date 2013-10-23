dd963c003729bc71fddfb2a6644c53345fafe325
Dustin Ryerson <dustin.ryerson@liferay.com>
2013-10-23 16:12:49 -0700

    LPS-41473

--- portal-impl/src/com/liferay/portal/service/permission/UserGroupRolePermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/UserGroupRolePermissionImpl.java
@@ -17,10 +17,13 @@ package com.liferay.portal.service.permission;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 
 /**
  * @author Brian Wing Shun Chan
@@ -44,6 +47,23 @@ public class UserGroupRolePermissionImpl implements UserGroupRolePermission {
 		throws PortalException, SystemException {
 
 		Group group = GroupLocalServiceUtil.getGroup(groupId);
+
+		Role role = RoleLocalServiceUtil.getRole(roleId);
+
+		if (!permissionChecker.isCompanyAdmin() &&
+			!permissionChecker.isGroupOwner(groupId)) {
+
+			String roleName = role.getName();
+
+			if (roleName.equals(
+					RoleConstants.ORGANIZATION_ADMINISTRATOR) ||
+				roleName.equals(RoleConstants.ORGANIZATION_OWNER) ||
+				roleName.equals(RoleConstants.SITE_ADMINISTRATOR) ||
+				roleName.equals(RoleConstants.SITE_OWNER)) {
+
+				return false;
+			}
+		}
 
 		if (permissionChecker.isGroupOwner(groupId) ||
 			GroupPermissionUtil.contains(
--- portal-impl/src/com/liferay/portlet/usersadmin/util/UsersAdminImpl.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/util/UsersAdminImpl.java
@@ -189,10 +189,10 @@ public class UsersAdminImpl implements UsersAdmin {
 		while (itr.hasNext()) {
 			Role groupRole = itr.next();
 
-			String name = groupRole.getName();
+			String roleName = groupRole.getName();
 
-			if (name.equals(RoleConstants.ORGANIZATION_USER) ||
-				name.equals(RoleConstants.SITE_MEMBER)) {
+			if (roleName.equals(RoleConstants.ORGANIZATION_USER) ||
+				roleName.equals(RoleConstants.SITE_MEMBER)) {
 
 				itr.remove();
 			}
@@ -209,13 +209,13 @@ public class UsersAdminImpl implements UsersAdmin {
 		while (itr.hasNext()) {
 			Role groupRole = itr.next();
 
-			String groupRoleName = groupRole.getName();
+			String roleName = groupRole.getName();
 
-			if (groupRoleName.equals(
+			if (roleName.equals(
 					RoleConstants.ORGANIZATION_ADMINISTRATOR) ||
-				groupRoleName.equals(RoleConstants.ORGANIZATION_OWNER) ||
-				groupRoleName.equals(RoleConstants.SITE_ADMINISTRATOR) ||
-				groupRoleName.equals(RoleConstants.SITE_OWNER)) {
+				roleName.equals(RoleConstants.ORGANIZATION_OWNER) ||
+				roleName.equals(RoleConstants.SITE_ADMINISTRATOR) ||
+				roleName.equals(RoleConstants.SITE_OWNER)) {
 
 				itr.remove();
 			}
@@ -314,13 +314,13 @@ public class UsersAdminImpl implements UsersAdmin {
 		while (itr.hasNext()) {
 			Role role = itr.next();
 
-			String name = role.getName();
+			String roleName = role.getName();
 
-			if (name.equals(RoleConstants.GUEST) ||
-				name.equals(RoleConstants.ORGANIZATION_USER) ||
-				name.equals(RoleConstants.OWNER) ||
-				name.equals(RoleConstants.SITE_MEMBER) ||
-				name.equals(RoleConstants.USER)) {
+			if (roleName.equals(RoleConstants.GUEST) ||
+				roleName.equals(RoleConstants.ORGANIZATION_USER) ||
+				roleName.equals(RoleConstants.OWNER) ||
+				roleName.equals(RoleConstants.SITE_MEMBER) ||
+				roleName.equals(RoleConstants.USER)) {
 
 				itr.remove();
 			}
@@ -362,10 +362,10 @@ public class UsersAdminImpl implements UsersAdmin {
 
 			Role role = userGroupRole.getRole();
 
-			String name = role.getName();
+			String roleName = role.getName();
 
-			if (name.equals(RoleConstants.ORGANIZATION_USER) ||
-				name.equals(RoleConstants.SITE_MEMBER)) {
+			if (roleName.equals(RoleConstants.ORGANIZATION_USER) ||
+				roleName.equals(RoleConstants.SITE_MEMBER)) {
 
 				itr.remove();
 			}
