f8f50f16325bc153c7fef1f2e6d168e4e09e4d39
Kenneth Chang <Kenneth.Chang@liferay.com>
2013-10-29 11:49:16 -0700

    LPS-40995 Source formatting

--- portal-impl/src/com/liferay/portlet/messageboards/action/ActionUtil.java
+++ portal-impl/src/com/liferay/portlet/messageboards/action/ActionUtil.java
@@ -21,7 +21,6 @@ import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.WebKeys;
 import com.liferay.portlet.messageboards.model.MBCategory;
-import com.liferay.portlet.messageboards.model.MBCategoryConstants;
 import com.liferay.portlet.messageboards.model.MBMessage;
 import com.liferay.portlet.messageboards.model.MBThread;
 import com.liferay.portlet.messageboards.service.MBBanLocalServiceUtil;
@@ -62,9 +61,7 @@ public class ActionUtil {
 
 		MBCategory category = null;
 
-		if ((categoryId > 0) &&
-			(categoryId != MBCategoryConstants.DEFAULT_PARENT_CATEGORY_ID)) {
-
+		if (categoryId > 0) {
 			category = MBCategoryServiceUtil.getCategory(categoryId);
 		}
 		else {
