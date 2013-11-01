9f9aebd1ed732faa4106cf0899511c1bf228da3d
Hugo Huijser <hugo.huijser@liferay.com>
2013-11-01 13:00:44 +0800

    LPS-41839 Rename error messages

--- portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
+++ portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
@@ -1264,7 +1264,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 			if (trimmedLine.startsWith(StringPool.EQUAL)) {
 				processErrorMessage(
-					fileName, "equal: " + fileName + " " + lineCount);
+					fileName, "line break: " + fileName + " " + lineCount);
 			}
 
 			if (line.contains("ActionForm form")) {
@@ -1724,7 +1724,7 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 
 							processErrorMessage(
 								fileName,
-								"new line: " + fileName + " " + lineCount);
+								"line break: " + fileName + " " + lineCount);
 						}
 					}
 
