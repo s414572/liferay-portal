9dc1cb2ae0334cf9d2693a75e6be30b22ab905d6
Dustin Ryerson <dustin.ryerson@liferay.com>
2013-11-13 08:35:45 -0800

    LPS-31528 Copyright

--- portal-impl/src/com/liferay/portlet/search/action/ConfigurationActionImpl.java
+++ portal-impl/src/com/liferay/portlet/search/action/ConfigurationActionImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -89,7 +89,7 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
 			String fieldName = oldFacetJSONObject.getString("fieldName");
 
 			if ((displayAssetCategoriesFacet &&
-					fieldName.equals("assetCategoryTitles")) ||
+				 fieldName.equals("assetCategoryTitles")) ||
 				(displayAssetTagsFacet && fieldName.equals("entryClassName")) ||
 				(displayAssetTypeFacet && fieldName.equals("assetTagNames")) ||
 				(displayModifiedRangeFacet && fieldName.equals("modified"))) {
